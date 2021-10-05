0b53d52011cdaa3115b1b6465e372779468e79f1
omar17md <86132736+omar17md@users.noreply.github.com>
2021-10-04 19:22:03 -0600

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,20 @@ import json
 
 def generar():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'https://apinasa2021.azurewebsites.net/')
+    r = http.request('GET', 'https://nasaapistellarbytes.azurewebsites.net/')
     s = json.loads(r.data)
     cadena = str(s)
     cadena = cadena.replace('\'', '"')
     arc = open('static/js/data.json', 'w')
     arc.write('data  = \'' + cadena  + '\';')
+    arc.close()
+
+def filtrar(id_norad):
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'https://nasaapistellarbytes.azurewebsites.net/id/' + str(id_norad))
+    s = json.loads(r.data)
+    cadena = str(s)
+    cadena = cadena.replace('\'', '"')
+    arc = open('static/js/dataf.json', 'w')
+    arc.write('data1  = \'' + cadena  + '\';')
     arc.close()
