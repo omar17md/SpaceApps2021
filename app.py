62b61c46955a98ddcbd72a53dde7b46c4f32cf01
omar17md <86132736+omar17md@users.noreply.github.com>
2021-10-04 19:21:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 import test
 
 app = Flask(__name__)
 
-test.generar()
-
 @app.route("/",methods = ['GET','POST'])
 def page():
+    test.generar()
     return render_template("home.html")
+
+@app.route("/buscar",methods = ['GET'])
+def procesar():
+    pr = request.args.get('id')
+    test.filtrar(pr)
+    return render_template("redireccionar.html")
+
+@app.route("/ver")
+def search():
+    return render_template("homeb.html")
+
+@app.route("/ocultar")
+def ocult():
+    return render_template("oculto.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
